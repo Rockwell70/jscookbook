41d34f2788472d63d2f83297a765fd0e2c1d1857
Rockwell70 <rockwellshabani@gmail.com>
2022-07-25 06:28:03 -0700

    added axios feed

--- 13-remote-data/parse-xml/index.js
+++ 13-remote-data/parse-xml/index.js
@@ -1,5 +1,8 @@
+// import fetch from "node-fetch"
+//const fetch = require("node-fetch")
+
 (async () => {
-  //const url = 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+  // const url = 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml';
   const url = 'http://www.axios.com/feeds/feed.rss';
 
   // fetch and parse the XML document
