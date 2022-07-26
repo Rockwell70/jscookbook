a9d7cbb8f8084a9dcd4f86cebcc8016fa9e7834c
Rockwell70 <rockwellshabani@gmail.com>
2022-07-25 17:11:20 -0700

    package update

--- 13-remote-data/parse-xml/index.js
+++ 13-remote-data/parse-xml/index.js
@@ -1,5 +1,5 @@
-import fetch from "node-fetch";
-// const {fetch} = require('node-fetch');
+// import fetch from "node-fetch";
+const {fetch} = require('node-fetch');
 
 (async () => {
   const url = 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml';
